579b52ace93fcbf5c3cdefd3551b92344aae4407
Maks Babenko <maks@babenoff.ru>
2024-10-08 00:37:20 +0300

    initial commit

---
+++ src/liblikedimion.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * @brief A Powerful C++ Library for High-Performance Numerical Computing
+ * @version 0.1.0
+ * @date 2024-09-21
+ * @authors alxvdev
+ * @copyright Apache 2.0 License
+ */
+#include <iostream>
+
+void println(std::string string) {
+    std::cout << string << std::endl;
+}
